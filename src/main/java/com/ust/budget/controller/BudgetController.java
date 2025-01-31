72401b78b0108a9e9dafbc3c4de09242ed8da34c
arjun.vinod <arjunvinod09@gmail.com>
2025-01-31 19:51:46 +0530

    CORS allowed

--- src/main/java/com/ust/budget/controller/BudgetController.java
+++ src/main/java/com/ust/budget/controller/BudgetController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/budget")
 public class BudgetController {
